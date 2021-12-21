1fb133574104ba243dbdc0a04d400ed8db2b78b0
ElisaSalvatore <elisa.salvatore110@gmail.it>
2021-12-21 18:29:51 +0100

    method2: 5 prompts added line 37

--- js/script.js
+++ js/script.js
@@ -8,10 +8,10 @@
 //costanti e variabili per generare 5 numeri random
 const containerHtml = document.querySelector('.container');
 const outputNumbers = document.querySelector('.numbers');
-const simonNumbers = [];
-const userNumbers = []; 
-let i;
+const ouputWinner = document.querySelector('.winner');
 
+const simonNumbersArray = [];
+let i;
 
 //funzione per generare numeri random
 function getRandom(min, max) {
@@ -19,33 +19,44 @@ function getRandom(min, max) {
 };
 
 //ciclo per stampare in pagina numeri random univoci da 1 a 100
-while (simonNumbers.length < 5) {
+while (simonNumbersArray.length < 5) {
     const randomNumber = getRandom (1, 100);
 
-    if (!simonNumbers.includes(randomNumber)) {
-        simonNumbers.push(randomNumber);
+    if (!simonNumbersArray.includes(randomNumber)) {
+        simonNumbersArray.push(randomNumber);
     };
 };
-outputNumbers.innerHTML = (simonNumbers);
-console.log(simonNumbers);
+outputNumbers.innerHTML = (simonNumbersArray);
+console.log(simonNumbersArray);
 
 //dopo 30sec far sparire i numeri utilizzando una timing function
 setTimeout(function (){
     containerHtml.remove(outputNumbers);
     
-    //far appararire un prompt in cui viene chiesto all'untente di inserire i numeri memorizzati 
-    const userNumbers = prompt('Inserisci la sequenza di numeri (lascia uno spazio tra un numero e l\'altro): ');
-    const splitUser = userNumbers.split(' ');
-    console.log(splitUser)
-
+    //far appararire un prompt per ogni numero in cui viene chiesto all'untente di inserire i numeri memorizzati 
+    const number1 = parseInt(prompt('Inserisci il primo numero della sequenza: '));
+    const number2 = parseInt(prompt('Inserisci il secondo numero della sequenza: '));
+    const number3 = parseInt(prompt('Inserisci il terzo numero della sequenza: '));
+    const number4 = parseInt(prompt('Inserisci il quarto numero della sequenza: '));
+    const number5 = parseInt(prompt('Iinserisci il quinto numero della sequenza: '));
+    
+    const userNumbersArray = [number1,number2,number3, number4, number5]
+    console.log(userNumbersArray);
+    
     //verifica dei numeri inseriti dall'utente
-    // const splitSimon = simonNumbers.split(' ');
-    // console.log(splitSimon)
-    if (splitUser == simonNumbers) {
-        console.log('hai vinto')
-    } else {
-        console.log('hai perso')
-    };
+    // if (userNumbersArray[i] === simonNumbersArray[i]){
+    //     console.log('L\'utente ha inserito la giusta sequenza.')
+    //     ouputWinner.innerHTML += 'Complimenti, hai inserito la corretta sequenza di numeri!'
+    // } else {
+    //     console.log('L\'utente ha inserito un\'errata sequenza.')
+    //     ouputWinner.innerHTML += 'Riprova, non hai inserito la corretta sequenza di numeri.'
+    // };
 },3000);
+
+
+
+
+
+
 
 
