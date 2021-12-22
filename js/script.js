888d0225236752e7dd93b715d20258d40be24b5f
ElisaSalvatore <elisa.salvatore110@gmail.it>
2021-12-22 17:42:48 +0100

    Add: line 76 .join()

--- js/script.js
+++ js/script.js
@@ -14,25 +14,28 @@ const buttonNewGame = document.querySelector('.button');
 
 const simonNumbersArray = [];
 
+let userInsertNumbers = [];
+let userCorrectNumbers = 0;
+
+// function newGame(randomNumber){  
+
+// };
+
 //FUNZIONE per generare numeri random
 function getRandom(min, max) {
     return Math.round(Math.random() * (max - min) + min);
 };
-
-// function newGame(randomNumber){  
-// };
-
 //GENERARE 5 NUMERI RANDOM
 while (simonNumbersArray.length < 5) {
     const randomNumber = getRandom (1, 100);
 
-    //se il randomNumber generato (!) NON è incluso in simonNumbersArray allora pushalo al suo interno
+    //se il randomNumber generato (!)NON è incluso in simonNumbersArray allora lo pusho al suo interno
     if (!simonNumbersArray.includes(randomNumber)) {
         simonNumbersArray.push(randomNumber);
     };
 };
 //STAMPARE IN PAGINA I 5 NUMERI GENERATI
-//aggiungere il contatore per incrementare l'array che contiene i numeri random (generarti dalla funzione getRandom)
+//aggiungere il contatore per incrementare simonNumbersArray che contiene i numeri random (generarti dalla funzione getRandom)
 for (let i = 0; i < simonNumbersArray.length; i++) {
     outputNumbers.innerHTML += simonNumbersArray[i] + ' ';
 }
@@ -45,19 +48,17 @@ setTimeout(function (){
     outputNumbers.innerHTML = ' ';
 
     setTimeout(function (){
-        //far appararire un prompt per ogni numero in cui viene chiesto all'untente di inserire i numeri memorizzati 
+        //far appararire un prompt, per ogni numero, in cui viene chiesto all'utente di inserire i numeri memorizzati 
         // oppure creare un contatore che ripeta l'azione del prompt per 5 volte (n random = n da inserire)
-        let userInsertNumbers = [];
-        let userCorrectNumbers = 0;
         
         for (i = 0; i < 5; i++) {
             const userAnswer = parseInt(prompt('Inserisci un numero della sequenza: '));
             //SE l'userAnswer è presente nella simonSaysArray E SE NON è incluso nella lista userInsertNumber, allora 
-            // aggiungi il numero inserito incrementando la lista userCorrectNumbers
-
+            // aggiungi il numero inserito incrementando userCorrectNumbers
             if (simonNumbersArray.includes(userAnswer) && !userInsertNumbers.includes(userAnswer)){
                 userCorrectNumbers++;
             };
+
             //in generale se i numeri inseriti (userAnswer) sono presenti o meno nella sequenza iniziale, 
             // sono comunque da aggiungere alla lista userInsertNumbers: 
             // in modo da verificare QUANTI numeri inseriti dall'utente sono corretti
@@ -67,10 +68,12 @@ setTimeout(function (){
         
         //mostrare nuovamente in pagina i numeri della sequenza inziale da memorizzare ed il punteggio (quanti numeri l'utente ha indovinato)
         outputNumbers.innerHTML += simonNumbersArray;
-        outputScore.innerHTML = `Hai indovinato ${userCorrectNumbers} numeri! La sequenza da te memorizzata è: ${(userInsertNumbers + ' ')}`;
+        outputScore.innerHTML = `Hai indovinato ${userCorrectNumbers} numeri! La sequenza da te inserita è: ${(userInsertNumbers + ' ')}`;
 
-        // verifica dei numeri inseriti dall'utente, se l'utente ha indovinato 5 numeri
-        if (userInsertNumbers[i] === simonNumbersArray[i]) {
+        //verifica dei numeri inseriti dall'utente, se l'utente ha indovinato TUTTI i 5 numeri della sequenza iniziale
+        // Utilizzando join() trasformo i due array in stringhe: inserirsco una virgola come separatore tra gli elementi e 
+        // confronto la similarità delle due stringhe
+        if ( userInsertNumbers.join(',') ===  simonNumbersArray.join(',')) {
             console.log('L\'utente ha inserito la giusta sequenza.')
             outputWinner.innerHTML += 'Complimenti, hai inserito la corretta sequenza di numeri!'
             
@@ -85,5 +88,5 @@ setTimeout(function (){
 
 // Capire quale funzione inserire per resettare la partita
 // buttonNewGame.addEventListener('click', function(){
-//     outputNumbers.innerHTML = newGame(randomNumber);
+//     newGame(randomNumber);
 // });
